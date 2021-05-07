cfee89d5f36c8a1efe15396097a0ea7e4ac39020
Jasonlcy3 <JasonLCY3@gmail.com>
2021-05-07 18:16:48 +0800

    Replace obsolete function for Qt 6

--- src/qjsonwebtoken.cpp
+++ src/qjsonwebtoken.cpp
@@ -186,9 +186,9 @@ bool QJsonWebToken::setAlgorithmStr(const QString &strAlgorithm)
 	// set algorithm
 	m_strAlgorithm = strAlgorithm;
 	// modify header
-    m_jdocHeader = QJsonDocument::fromJson(QString::fromUtf8("{\"typ\": \"JWT\", \"alg\" : \"").toUtf8()
+    m_jdocHeader = QJsonDocument::fromJson(QByteArray("{\"typ\": \"JWT\", \"alg\" : \"")
 		                                 + m_strAlgorithm.toUtf8()
-                                         + QString::fromUtf8("\"}").toUtf8());
+                                         + QByteArray("\"}"));
 
 	return true;
 }
